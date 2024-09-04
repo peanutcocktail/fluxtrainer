4c3be0a1c29bf5225fe95d023a8185882fea740a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-09-04 11:02:27 -0400

    update

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def create_dataset(size, *inputs):
 
 
 def run_captioning(images, concept_sentence, *captions):
-    print(f"run_captioning")
+    print(f"run_captioning, {images}, {concept_sentence}")
+    print(f"captions {captions}")
     #Load internally to not consume resources for training
     device = "cuda" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16
